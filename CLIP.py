6e189762f4fce5f503b9daa5224abd898ae8ef9d
Kerllose George <82047749+NR-Kagero@users.noreply.github.com>
2026-01-14 20:29:05 +0200

    Add files via upload

---
+++ CLIP.py
@@ -0,0 +1,11 @@
+import torch
+from transformers import pipeline
+
+clip = pipeline(
+   task="zero-shot-image-classification",
+   model="openai/clip-vit-base-patch32",
+   dtype=torch.bfloat16,
+   device=0
+)
+labels = ["a photo of a cat", "a photo of a dog", "a photo of a car"]
+print(clip(r"C:\Users\Kagero\PycharmProjects\Multimodal\images.webp", candidate_labels=labels))
---
+++ PLIP.py
@@ -0,0 +1,19 @@
+import requests
+import torch
+from PIL import Image
+from transformers import AutoProcessor, AutoModelForVisualQuestionAnswering
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = AutoModelForVisualQuestionAnswering.from_pretrained(
+    "Salesforce/blip-vqa-base",
+    dtype=torch.float16,
+    device_map="auto"
+)
+
+image = Image.open(r"C:\Users\Kagero\PycharmProjects\Multimodal\download.jpg")
+
+question = "What is the weather in this image?"
+inputs = processor(images=image, text=question, return_tensors="pt").to(model.device, torch.float16)
+
+output = model.generate(**inputs)
+print(processor.batch_decode(output, skip_special_tokens=True)[0])
